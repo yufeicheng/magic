f438714e4677654e8b3a92005a4ff7a39b468f7d
ChengYufei <873401580@qq.com>
2019-12-15 18:22:09 +0800

    interview - 遍历目录下某后缀文件替换文本

--- magic-interview/src/main/java/com/magic/interview/service/dynamic_datasource/ReplaceService.java
+++ magic-interview/src/main/java/com/magic/interview/service/dynamic_datasource/ReplaceService.java
@@ -1,7 +1,11 @@
 package com.magic.interview.service.dynamic_datasource;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.base.Supplier;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
@@ -36,65 +40,74 @@ public class ReplaceService {
      */
     public static void scanFils(String path, String suffix, String beReplacedWord, String newWord) throws IOException {
 
-        final Path[] cur = new Path[1];
-        final Set<Integer>[] lines = new Set[1];
-        final int[] i = {0};
+        final Path[] currentPath = new Path[1];
+        final Set<Integer>[] modifyLines = new Set[1];
+        final int[] count = {0};
         final StringBuffer[] stringBuffer = new StringBuffer[1];
+        final long[] total = {0};
 
         Map<String, Set<Integer>> result = new HashMap<>();
-        Map<String, StringBuffer> data = new HashMap<>();
+        Map<Path, StringBuffer> data = new HashMap<>();
         Files.walk(Paths.get(path)).filter(p -> !Files.isDirectory(p))
                 .filter(p -> {
                     boolean matches = FileSystems.getDefault().getPathMatcher("glob:**.{" + suffix + "}").matches(p);
                     if (matches) {
                         stringBuffer[0] = new StringBuffer();
-                        cur[0] = p;
-                        i[0] = 0;
-                        lines[0] = new HashSet<>();
+                        currentPath[0] = p;
+                        count[0] = 0;
+                        modifyLines[0] = new HashSet<>();
                     }
                     return matches;
                 })
                 .flatMap(p -> {
-                    try {
-                        return Files.lines(p, Charset.forName("UTF-8"));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    return Stream.empty();
+                    Supplier<Stream<String>> streams = () -> {
+                        try {
+                            return Files.lines(p, Charset.forName("UTF-8"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return Stream.empty();
+                    };
+                    total[0] = streams.get().count();
+                    return streams.get();
                 })
-                .map(s -> {
+                .forEach(s -> {
                     if (!StringUtils.contains(s, beReplacedWord)) {
                         stringBuffer[0].append(s).append(System.lineSeparator());
-                        i[0]++;
-                        return stringBuffer[0].toString();
+                        count[0]++;
+                        if (count[0] == total[0] && CollectionUtils.isNotEmpty(modifyLines[0]) && ArrayUtils.isNotEmpty(stringBuffer)) {
+                            result.put(currentPath[0].getFileName().toString(), modifyLines[0]);
+                            data.put(currentPath[0], stringBuffer[0]);
+                        }
+                        return;
                     }
                     String replace = StringUtils.replace(s, beReplacedWord, newWord);
                     stringBuffer[0].append(replace).append(System.lineSeparator());
-                    i[0]++;
-                    lines[0].add(i[0]);
-                    return stringBuffer[0].toString();
-                })
-                .forEach(s -> {
-                    if (lines[0].size() <= 0) {
-                        return;
+                    count[0]++;
+                    modifyLines[0].add(count[0]);
+
+                    if (count[0] == total[0]) {
+                        result.put(currentPath[0].getFileName().toString(), modifyLines[0]);
+                        data.put(currentPath[0], stringBuffer[0]);
                     }
-                    System.out.println(cur[0].getFileName().toString() + "---" + lines[0].size());
-                    result.put(cur[0].getFileName().toString(), lines[0]);
-                    data.put(cur[0].getFileName().toString(), stringBuffer[0]);
                 });
+        if (MapUtils.isEmpty(data)) {
+            return;
+        }
+        //回写文件
         data.forEach((k, v) -> {
             try {
-                Files.write(cur[0], v.toString().getBytes());
+                Files.write(k, v.toString().getBytes());
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
         //输出执行结果文件
-        FileUtils.writeStringToFile(new File("D:/test/res.txt"), JSON.toJSONString(result), Charset.forName("UTF-8"));
+        FileUtils.writeStringToFile(new File("/Users/chengyufei/Downloads/dmg/ebook/res.txt"), JSON.toJSONString(result), Charset.forName("UTF-8"));
     }
 
     public static void main(String[] args) throws IOException {
-        scanFils("D:/test", "html", "itougu.jrj.com.cn", "www.itougu.com");
+        scanFils("/Users/chengyufei/Downloads/dmg/ebook", "txt", "itougu.jrj.com.cn", "www.itougu.com");
     }
 }
